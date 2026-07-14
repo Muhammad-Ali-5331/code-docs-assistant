81d41ac9b38831a7d8a65d681a6400dfbd98ecd1
Muhammad Ali <malitariq5324@gmail.com>
2026-07-14 09:03:26 +0500

    Refactor Firebase credentials handling and add Procfile for deployment

--- firestore_client.py
+++ firestore_client.py
@@ -3,9 +3,17 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 import os
+import json
 
-cred_path = os.getenv("FIREBASE_CREDENTIALS_PATH")
-cred = credentials.Certificate(cred_path)
+firebase_json = os.getenv("FIREBASE_CREDENTIALS_JSON")
+
+if firebase_json:
+    cred_dict = json.loads(firebase_json)
+    cred = credentials.Certificate(cred_dict)
+else:
+    cred_path = os.getenv("FIREBASE_CREDENTIALS_PATH")
+    cred = credentials.Certificate(cred_path)
+
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
